55ab12b0e7438724e7e567ed8855f1fe8fb4fede
BoitumeloL <34854304@mynwu.ac.za>
2023-05-18 18:43:35 +0200

    recursive Fibonacci, generate key, reverseConcatenate

--- Cryptron/Form2.cs
+++ Cryptron/Form2.cs
@@ -22,31 +22,78 @@ namespace Cryptron
 
         }
 
+        private string GenerateKey()
+        {
+            //Generate random key based on english alphabet
+            int byteKeyLegth = tbMessage.Text.Length;
+            byte[] length;
+            length = new byte[byteKeyLegth];
+            const string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var random = new RNGCryptoServiceProvider();
+
+            random.GetBytes(length);
+            var key = new StringBuilder();
+            foreach (var b in length)
+            {
+                int index = b % alphabet.Length;
+                key.Append(alphabet[index]);
+            }
+            return key.ToString();
+        }
 
         //fibonacci sequence
         public static int[] Fibonacci(int start, int nth)
         {
+            int number = 0;
             //fibonacci sequence that starts at the position of the start variable and has a length of the nth variable
             int[] seq = new int[nth];
-            if (start <= 1)
+            if(start <= 1)
             {
                 seq[0] = 0;
-                seq[1] = 1;
+                
             }
             else
             {
-                seq[0] = start;
-                seq[1] = start + 1;
-            }
-            for (int i = 2; i < nth; i++)
-            {
-                seq[i] = seq[i - 1] + seq[i - 2];
-                
+                for(int i = 0; i < nth; i++)
+                {
+                    number = Fib(start - 1) + Fib(start - 2);
+                    start++;
+                    seq[i] = number;
+
+                }   
             }
             return seq;
         }
-        
+        private static int Fib(int n) //recursive fibonacci sequence
+        {
+            if(n <= 1)
+            {
+                return n;
+            }
+            else
+            {
+                return Fib(n - 1) + Fib(n - 2);
+            }
+        }
 
+        private static string ReverseConcatenate(string text, string key)
+        {
+            char[] revKey = key.ToCharArray();
+            Array.Reverse(revKey);
+
+            StringBuilder wordbuilder = new StringBuilder();
+            for(int j = 0; j < text.Length; j++)
+            {
+                wordbuilder.Append(text[j]);
+               if(j < revKey.Length)
+                {
+                    wordbuilder.Append(revKey[j]);
+                }
+
+            }
+            return wordbuilder.ToString();
+
+        }
         
 
         private void button2_Click(object sender, EventArgs e)
@@ -64,37 +111,30 @@ namespace Cryptron
         private void btnEncrypt_Click(object sender, EventArgs e)
         {
             int[] fib = new int[tbMessage.Text.Length];
+            string plainText = tbMessage.Text;
+            string key = GenerateKey();
+            string cipherText = ReverseConcatenate(plainText, key);
+            
+            //MessageBox.Show(key);
             fib = Fibonacci(3, 6);
-            for(int i = 0; i < fib.Length; i++)
+            /*for(int i = 0; i < fib.Length; i++)
             {
                 tbText.Text += fib[i].ToString() + " ";
-            }
-            //tbText.Text = fib[0].ToString();
+            }*/
+            tbText.Text = "Plain text: " + plainText + "\nKey: " + key +  "\nCipherText before fibonacci cipher:" + cipherText;
+            
+
         }
 
         private void btnDecrypt_Click(object sender, EventArgs e)
         {
 
-            ;
+            
         }
 
         private void btnGenerateKey_Click(object sender, EventArgs e)
         {
-            //Generate random key based on english alphabet
-            int byteKeyLegth = tbMessage.Text.Length;
-            byte[] length;
-            length = new byte[byteKeyLegth];
-            const string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            var random = new RNGCryptoServiceProvider();
-
-            random.GetBytes(length);
-            var key = new StringBuilder();
-            foreach (var b in length)
-            {
-                int index = b % alphabet.Length;
-                key.Append(alphabet[index]);
-            }
-            MessageBox.Show(key.ToString());
+            
 
         }
         private void label1_Click(object sender, EventArgs e)
